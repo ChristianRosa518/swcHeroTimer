aed50d2b2972fb33b115dbfd06caafd2b36462aa
ChristianRosa518 <chrisrosa815518@gmail.com>
2023-06-03 10:23:26 -0400

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Continents from "./components/continent/Continents";
 
+const serverURL = import.meta.env.VITE_SERVER_URL;
+
 function App() {
   return (
     <>
+      {serverURL}
       <Continents />
     </>
   );
