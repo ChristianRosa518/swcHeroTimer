2811893121524ad7af321242c79b5e7293104b6d
ChristianRosa518 <chrisrosa815518@gmail.com>
2023-06-04 15:07:33 -0400

    changed deployment to vite srver

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import Continents from "./components/continent/Continents";
 
-// const serverURL = import.meta.env.VITE_SERVER_URL as string;
-const serverURL = "http://localhost:3001" as string;
+const serverURL = import.meta.env.VITE_SERVER_URL as string;
+// const serverURL = "http://localhost:3001" as string;
 
 function App() {
   return (
