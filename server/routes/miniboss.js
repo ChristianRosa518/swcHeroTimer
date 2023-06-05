8baad761d3ba15173060056d48291303c932b324
ChristianRosa518 <chrisrosa815518@gmail.com>
2023-06-05 00:31:04 -0400

    slight change

--- server/routes/miniboss.js
+++ server/routes/miniboss.js
@@ -4,59 +4,61 @@ const miniBoss = require("../models/mini");
 
 router.put("/updateMiniBoss", async (req, res) => {
   const name = req.body.name;
-  const data = await miniBoss.findOne({ continent: name });
-  //make new date to update time without large function
-  const newDate = new Date(
-    data.nextYear,
-    data.nextMonth,
-    data.nextDay,
-    data.nextHour,
-    data.nextMinute,
-    0,
-    0
-  );
 
-  // check if db time - current time is negative, if negative update new value with next value
-  // add additional hours for mb update. three hour schedule.
   const currentDate = new Date();
-  const mathedDate = newDate.getTime() - currentDate.getTime();
-  console.log(currentDate.getTime(), "currentDate Time");
-  console.log(newDate.getTime(), "newDate Time");
-  console.log(mathedDate, "minused Time");
+  try {
+    const data = await miniBoss.findOne({ continent: name });
 
-  if (mathedDate < 0) {
-    console.log("times updated");
-    newDate.setHours(newDate.getHours() + 3);
-    const [years, months, days, hours, minutes, seconds] =
-      getReturnValues(newDate);
+    const newDate = new Date(
+      data.nextYear,
+      data.nextMonth,
+      data.nextDay,
+      data.nextHour,
+      data.nextMinute,
+      0,
+      0
+    );
+    const mathedDate = newDate.getTime() - currentDate.getTime();
+    console.log(currentDate.getTime(), "currentDate Time");
+    console.log(newDate.getTime(), "newDate Time");
+    console.log(mathedDate, "minused Time");
 
-    // update values
-    data.nextYear = years;
-    data.nextMonth = months;
-    data.nextDay = days;
-    data.nextHour = hours;
-    data.nextMinute = minutes;
+    if (mathedDate < 0) {
+      console.log("times updated");
+      newDate.setHours(newDate.getHours() + 3);
+      const [years, months, days, hours, minutes, seconds] =
+        getReturnValues(newDate);
 
-    data.save();
-    // console.log(`${data.continent} Miniboss times updated`);
-    res.json({
-      years: years,
-      months: months,
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds,
-    });
-  } else if (mathedDate > 0) {
-    console.log("not saving data");
-    res.json({
-      years: data.nextYear,
-      months: data.nextMonth,
-      days: data.nextDay,
-      hours: data.nextHour,
-      minutes: data.nextMinute,
-      seconds: data.nextSecond,
-    });
+      // update values
+      data.nextYear = years;
+      data.nextMonth = months;
+      data.nextDay = days;
+      data.nextHour = hours;
+      data.nextMinute = minutes;
+
+      await data.save();
+      // console.log(`${data.continent} Miniboss times updated`);
+      res.json({
+        years: years,
+        months: months,
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+      });
+    } else if (mathedDate > 0) {
+      console.log("not saving data");
+      res.json({
+        years: data.nextYear,
+        months: data.nextMonth,
+        days: data.nextDay,
+        hours: data.nextHour,
+        minutes: data.nextMinute,
+        seconds: data.nextSecond,
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
